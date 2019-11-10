303b47d7f75114e950b2a42d3020bda4a72b9056
LockManipulator <LockManipulator@gmail.com>
2019-11-10 04:06:04 -0500

    Bug fix

--- src/hooks/display/change_stat_display.h
+++ src/hooks/display/change_stat_display.h
@@ -33,90 +33,166 @@ extern "C" float hp_stat_hook() { // mod 0
 	return 0.1f * (float)player->climbing_speed;
 }
 
+GETTER_VAR(void*, ASM_regen_stat_Hook_jmpback);
+__attribute__((naked)) void ASM_regen_stat_Hook() {
+	asm(".intel_syntax \n"
+		"call regen_stat_hook \n"
+		DEREF_JMP(ASM_regen_stat_Hook_jmpback)
+		".att_syntax \n"
+	);
+}
+
+GETTER_VAR(void*, ASM_damage_stat_Hook_jmpback);
+__attribute__((naked)) void ASM_damage_stat_Hook() {
+	asm(".intel_syntax \n"
+		"call damage_stat_hook \n"
+		DEREF_JMP(ASM_damage_stat_Hook_jmpback)
+		".att_syntax \n"
+	);
+}
+
+GETTER_VAR(void*, ASM_armor_stat_Hook_jmpback);
+__attribute__((naked)) void ASM_armor_stat_Hook() {
+	asm(".intel_syntax \n"
+		"call armor_stat_hook \n"
+		DEREF_JMP(ASM_armor_stat_Hook_jmpback)
+		".att_syntax \n"
+	);
+}
+
+GETTER_VAR(void*, ASM_resistance_stat_Hook_jmpback);
+__attribute__((naked)) void ASM_resistance_stat_Hook() {
+	asm(".intel_syntax \n"
+		"call resistance_stat_hook \n"
+		DEREF_JMP(ASM_resistance_stat_Hook_jmpback)
+		".att_syntax \n"
+	);
+}
+
+GETTER_VAR(void*, ASM_crit_stat_Hook_jmpback);
+__attribute__((naked)) void ASM_crit_stat_Hook() {
+	asm(".intel_syntax \n"
+		"call crit_stat_hook \n"
+		DEREF_JMP(ASM_crit_stat_Hook_jmpback)
+		".att_syntax \n"
+	);
+}
+
+GETTER_VAR(void*, ASM_haste_stat_Hook_jmpback);
+__attribute__((naked)) void ASM_haste_stat_Hook() {
+	asm(".intel_syntax \n"
+		"call haste_stat_hook \n"
+		DEREF_JMP(ASM_haste_stat_Hook_jmpback)
+		".att_syntax \n"
+	);
+}
+
+GETTER_VAR(void*, ASM_hp_stat_Hook_jmpback);
+__attribute__((naked)) void ASM_hp_stat_Hook() {
+	asm(".intel_syntax \n"
+		"call hp_stat_hook \n"
+		DEREF_JMP(ASM_hp_stat_Hook_jmpback)
+		".att_syntax \n"
+	);
+}
+
 
 void change_regen_display() {
 	char* base = (char*)CWBase();
 	int offset = 0x27554F;
-	WriteFarJMP(Offset(base, 0x5B6452), (void*)&regen_stat_hook);
+	WriteFarJMP(Offset(base, 0x5B6452), (void*)&ASM_regen_stat_Hook);
 
 	WriteByte(Offset(base, offset), 0xE9);
 	WriteByte(base + offset + 0x01, 0xFE);
 	WriteByte(base + offset + 0x02, 0x0E);
 	WriteByte(base + offset + 0x03, 0x34);
 	WriteByte(base + offset + 0x04, 0x00);
+
+	ASM_regen_stat_Hook_jmpback = (void*) (base + offset + 0x05);
 }
 
 void change_damage_display() {
 	char* base = (char*)CWBase();
-	int offset = 0x2755DF;
-	WriteFarJMP(Offset(base, 0x5B6444), (void*)&damage_stat_hook);
+	int offset = 0x2755D5;
+	WriteFarJMP(Offset(base, 0x5B6444), (void*)&ASM_damage_stat_Hook);
 
 	WriteByte(Offset(base, offset), 0xE9);
-	WriteByte(base + offset + 0x01, 0x60);
+	WriteByte(base + offset + 0x01, 0x6A);
 	WriteByte(base + offset + 0x02, 0x0E);
 	WriteByte(base + offset + 0x03, 0x34);
 	WriteByte(base + offset + 0x04, 0x00);
+
+	ASM_damage_stat_Hook_jmpback = (void*)(base + offset + 0x05);
 }
 
 void change_armor_display() {
 	char* base = (char*)CWBase();
 	int offset = 0x2755BC;
-	WriteFarJMP(Offset(base, 0x5B6436), (void*)&armor_stat_hook);
+	WriteFarJMP(Offset(base, 0x5B6436), (void*)&ASM_armor_stat_Hook);
 
 	WriteByte(Offset(base, offset), 0xE9);
 	WriteByte(base + offset + 0x01, 0x75);
 	WriteByte(base + offset + 0x02, 0x0E);
 	WriteByte(base + offset + 0x03, 0x34);
 	WriteByte(base + offset + 0x04, 0x00);
+
+	ASM_armor_stat_Hook_jmpback = (void*)(base + offset + 0x05);
 }
 
 void change_resistance_display() {
 	char* base = (char*)CWBase();
 	int offset = 0x275629;
-	WriteFarJMP(Offset(base, 0x5B6428), (void*)&resistance_stat_hook);
-
+	WriteFarJMP(Offset(base, 0x5B6428), (void*)&ASM_resistance_stat_Hook);
 	WriteByte(Offset(base, offset), 0xE9);
 	WriteByte(base + offset + 0x01, 0xFA);
 	WriteByte(base + offset + 0x02, 0x0D);
 	WriteByte(base + offset + 0x03, 0x34);
 	WriteByte(base + offset + 0x04, 0x00);
+
+	ASM_resistance_stat_Hook_jmpback = (void*)(base + offset + 0x05);
 }
 
 
 void change_crit_display() {
 	char* base = (char*)CWBase();
 	int offset = 0x2755A3;
-	WriteFarJMP(Offset(base, 0x5B641A), (void*)&crit_stat_hook);
+	WriteFarJMP(Offset(base, 0x5B641A), (void*)&ASM_crit_stat_Hook);
 
 	WriteByte(Offset(base, offset), 0xE9);
 	WriteByte(base + offset + 0x01, 0x72);
 	WriteByte(base + offset + 0x02, 0x0E);
 	WriteByte(base + offset + 0x03, 0x34);
 	WriteByte(base + offset + 0x04, 0x00);
+
+	ASM_crit_stat_Hook_jmpback = (void*)(base + offset + 0x05);
 }
 
 void change_haste_display() {
 	char* base = (char*)CWBase();
 	int offset = 0x27560D;
-	WriteFarJMP(Offset(base, 0x5B640C), (void*)&haste_stat_hook);
+	WriteFarJMP(Offset(base, 0x5B640C), (void*)&ASM_haste_stat_Hook);
 
 	WriteByte(Offset(base, offset), 0xE9);
 	WriteByte(base + offset + 0x01, 0xFA);
 	WriteByte(base + offset + 0x02, 0x0D);
 	WriteByte(base + offset + 0x03, 0x34);
 	WriteByte(base + offset + 0x04, 0x00);
+
+	ASM_haste_stat_Hook_jmpback = (void*)(base + offset + 0x05);
 }
 
 void change_hp_display() {
 	char* base = (char*)CWBase();
 	int offset = 0x27558A;
-	WriteFarJMP(Offset(base, 0x5B63FE), (void*)&hp_stat_hook);
+	WriteFarJMP(Offset(base, 0x5B63FE), (void*)&ASM_hp_stat_Hook);
 
 	WriteByte(Offset(base, offset), 0xE9);
 	WriteByte(base + offset + 0x01, 0x6F);
 	WriteByte(base + offset + 0x02, 0x0E);
 	WriteByte(base + offset + 0x03, 0x34);
 	WriteByte(base + offset + 0x04, 0x00);
+
+	ASM_hp_stat_Hook_jmpback = (void*)(base + offset + 0x05);
 }
 
 void remove_roundf_stat_display() {
